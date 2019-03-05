7135e99670bd8ef59e4b380a6213f6fd0c615ebd
Juan David Torres <juan.torres.canon@correounivalle.edu.co>
2019-03-05 08:42:43 -0500

    Mod arbol y añadir bogosort

--- Arbol.py
+++ Arbol.py
@@ -30,26 +30,18 @@ def pesoPromArbol(arbol):
 
 def alturaArbol(arbol):
 	if type(arbol) == int:
+		print("IF 1")
 		return 1
 	elif len(arbol) == 1:
+		print("IF 2")
 		return alturaArbol(arbol[0])
 	elif len(arbol) == 2 and type(arbol[0]) == int:
+		print("IF 3")
 		return 1+max(alturaArbol(arbol[0]),alturaArbol(arbol[1]))
 	else:
 		result = []
 		n = len(arbol)
 		for i in range(n):
+			print("FOR")
 			result.append(alturaArbol(arbol[i]))
 		return max(result)
-
-
-arbol1 = [4, [[1],[2,[3]],[5,[[1],[4]]]]]
-arbol2 = [4]
-arbol3 = [4,[[2],[1]]]
-
-peso = pesoArbol(arbol1)
-altura = alturaArbol(arbol1)
-pesoProm = pesoPromArbol(arbol1)
-print(peso)
-print(pesoProm)
-print(altura)
